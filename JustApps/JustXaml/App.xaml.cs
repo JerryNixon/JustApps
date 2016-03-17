5f9fc1dec460eb33a2e66cb89a5a0cb2869f7a62
jerrynixon <jerry@nixoncorp.com>
2016-03-16 23:06:01 -0600

    Associated

--- JustApps/JustXaml/App.xaml.cs
+++ JustApps/JustXaml/App.xaml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
 
@@ -18,7 +20,22 @@ namespace JustXaml
 
         public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
-            NavigationService.Navigate(typeof(Views.MainPage));
+            var root = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var folder = await root.GetFolderAsync("Templates");
+            var list = new List<Models.Group>();
+            foreach (var subfolder in await folder.GetFoldersAsync())
+            {
+                var files = await subfolder.GetFilesAsync();
+                var group = new Models.Group
+                {
+                    Title = subfolder.Name,
+                    Items = files.Select(x => new Models.Item { Title = x.DisplayName, Path = x.Path }),
+                };
+                list.Add(group);
+            }
+
+            SessionState[nameof(JustXaml)] = list;
+            NavigationService.Navigate(typeof(Views.MainPage), nameof(JustXaml));
             await Task.CompletedTask;
         }
     }
--- JustApps/JustXaml/ViewModels/MainPageViewModel.cs
+++ JustApps/JustXaml/ViewModels/MainPageViewModel.cs
@@ -12,20 +12,11 @@ namespace JustXaml.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
         {
-            var root = Windows.ApplicationModel.Package.Current.InstalledLocation;
-            var folder = await root.GetFolderAsync("Templates");
-            foreach (var subfolder in await folder.GetFoldersAsync())
-            {
-                var files = await subfolder.GetFilesAsync();
-                var group = new Models.Group
-                {
-                    Title = subfolder.Name,
-                    Items = files.Select(x => new Models.Item { Title = x.DisplayName, Path = x.Path }),
-                };
-                Items.Add(group);
-            }
+            var p = SessionState[parameter.ToString()] as List<Models.Group>;
+            p.ForEach(x => Items.Add(x));
+            return Task.CompletedTask;
         }
 
         public ObservableCollection<Models.Group> Items { get; } = new ObservableCollection<Models.Group>();
